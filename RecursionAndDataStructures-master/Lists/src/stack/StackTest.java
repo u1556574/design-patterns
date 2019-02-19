9c365d70424a676301e9a015b97de8db05f02c9e
u1556574 <u1556574@hud.ac.uk>
2019-02-19 11:19:10 +0000

    test1

--- RecursionAndDataStructures-master/Lists/src/stack/StackTest.java
+++ RecursionAndDataStructures-master/Lists/src/stack/StackTest.java
@@ -8,12 +8,10 @@ public class StackTest {
 
     public static void main(String args[]) {
         Stack s = new Stack();
-
         s.addEntry("This");
         s.addEntry("Is");
         s.addEntry("A");
         s.addEntry("Test");
-
         while (true) {
             try{
                 System.out.println(s.getEntry());
